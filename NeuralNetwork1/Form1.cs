b99f3aeff6a836aa34b1d148a5bd5430b6505c52
Bostan Igor <bostanigor@gmail.com>
2020-12-11 10:06:34 +0300

    Initial commit

---
+++ NeuralNetwork1/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NeuralNetwork1
+{
+
+	public delegate void FormUpdater(double progress, double error, TimeSpan time);
+
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Генератор изображений (образов)
+        /// </summary>
+        GenerateImage generator = new GenerateImage();
+        
+        /// <summary>
+        /// Самодельный персептрон – из массивов и палок
+        /// </summary>
+        NeuralNetwork CustomNet = null;
+
+        /// <summary>
+        /// Обёртка для ActivationNetwork из Accord.Net
+        /// </summary>
+        AccordNet AccordNet = null;
+
+        /// <summary>
+        /// Абстрактный базовый класс, псевдоним либо для CustomNet, либо для AccordNet
+        /// </summary>
+        BaseNetwork net = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            netTypeBox.SelectedIndex = 1;
+            generator.figure_count = (int)classCounter.Value;
+            button3_Click(this, null);
+            pictureBox1.Image = Properties.Resources.Title;
+        }
+
+		public void UpdateLearningInfo(double progress, double error, TimeSpan elapsedTime)
+		{
+			if (progressBar1.InvokeRequired)
+			{
+				progressBar1.Invoke(new FormUpdater(UpdateLearningInfo),new Object[] {progress, error, elapsedTime});
+				return;
+			}
+            StatusLabel.Text = "Accuracy: " + error.ToString();
+            int prgs = (int)Math.Round(progress*100);
+			prgs = Math.Min(100, Math.Max(0,prgs));
+            elapsedTimeLabel.Text = "Затраченное время : " + elapsedTime.Duration().ToString(@"hh\:mm\:ss\:ff");
+            progressBar1.Value = prgs;
+		}
+
+
+        private void set_result(Sample figure)
+        {
+            label1.Text = figure.ToString();
+
+            if (figure.Correct())
+                label1.ForeColor = Color.Green;
+            else
+                label1.ForeColor = Color.Red;
+
+            label1.Text = "Распознано : " + figure.recognizedClass.ToString();
+
+            label8.Text = String.Join("\n", net.getOutput().Select(d => d.ToString()));
+            pictureBox1.Image = generator.genBitmap();
+            pictureBox1.Invalidate();
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Sample fig = generator.GenerateFigure();
+
+            net.Predict(fig);
+
+            set_result(fig);
+        }
+
+        private async Task<double> train_networkAsync(int training_size, int epoches, double acceptable_error, bool parallel = true)
+        {
+            //  Выключаем всё ненужное
+            label1.Text = "Выполняется обучение...";
+            label1.ForeColor = Color.Red;
+            groupBox1.Enabled = false;
+            pictureBox1.Enabled = false;
+            trainOneButton.Enabled = false;
+
+            //  Создаём новую обучающую выборку
+            SamplesSet samples = new SamplesSet();
+
+            for (int i = 0; i < training_size; i++)
+                samples.AddSample(generator.GenerateFigure());
+
+            //  Обучение запускаем асинхронно, чтобы не блокировать форму
+            double f = await Task.Run(() => net.TrainOnDataSet(samples, epoches, acceptable_error, parallel));
+
+            label1.Text = "Щелкните на картинку для теста нового образа";
+            label1.ForeColor = Color.Green;
+            groupBox1.Enabled = true;
+            pictureBox1.Enabled = true;
+            trainOneButton.Enabled = true;
+            StatusLabel.Text = "Accuracy: " + f.ToString();
+            StatusLabel.ForeColor = Color.Green;
+            return f;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            train_networkAsync( (int)TrainingSizeCounter.Value, (int)EpochesCounter.Value, (100 - AccuracyCounter.Value) / 100.0, parallelCheckBox.Checked);
+            #pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            //  Тут просто тестирование новой выборки
+            //  Создаём новую обучающую выборку
+            SamplesSet samples = new SamplesSet();
+
+            for (int i = 0; i < (int)TrainingSizeCounter.Value; i++)
+                samples.AddSample(generator.GenerateFigure());
+
+            double accuracy = net.TestOnDataSet(samples);
+            
+            StatusLabel.Text = string.Format("Точность на тестовой выборке : {0,5:F2}%", accuracy*100);
+            if (accuracy*100 >= AccuracyCounter.Value)
+                StatusLabel.ForeColor = Color.Green;
+            else
+                StatusLabel.ForeColor = Color.Red;
+
+            this.Enabled = true;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //  Проверяем корректность задания структуры сети
+            int[] structure = netStructureBox.Text.Split(';').Select((c) => int.Parse(c)).ToArray();
+            if (structure.Length < 2 || structure[0] != 400 || structure[structure.Length - 1] != generator.figure_count)
+            {
+                MessageBox.Show("А давайте вы структуру сети нормально запишите, ОК?", "Ошибка", MessageBoxButtons.OK);
+                return;
+            };
+
+            CustomNet = new NeuralNetwork(structure);
+            CustomNet.updateDelegate = UpdateLearningInfo;
+
+            AccordNet = new AccordNet(structure);
+            AccordNet.updateDelegate = UpdateLearningInfo;
+
+            net = CustomNet;
+        }
+
+        private void classCounter_ValueChanged(object sender, EventArgs e)
+        {
+            generator.figure_count = (int)classCounter.Value;
+            var vals = netStructureBox.Text.Split(';');
+            int outputNeurons;
+            if (int.TryParse(vals.Last(),out outputNeurons))
+            {
+                vals[vals.Length - 1] = classCounter.Value.ToString();
+                netStructureBox.Text = vals.Aggregate((partialPhrase, word) => $"{partialPhrase};{word}");
+            }
+        }
+
+        private void btnTrainOne_Click(object sender, EventArgs e)
+        {
+            if (net == null) return;
+            Sample fig = generator.GenerateFigure();
+            pictureBox1.Image = generator.genBitmap();
+            pictureBox1.Invalidate();
+            net.Train(fig, false);
+            set_result(fig);
+        }
+
+        private void netTrainButton_MouseEnter(object sender, EventArgs e)
+        {
+            infoStatusLabel.Text = "Обучить нейросеть с указанными параметрами";
+        }
+
+        private void testNetButton_MouseEnter(object sender, EventArgs e)
+        {
+            infoStatusLabel.Text = "Тестировать нейросеть на тестовой выборке такого же размера";
+        }
+
+        private void netTypeBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (netTypeBox.SelectedIndex == 0)
+                net = CustomNet;
+            else
+                net = AccordNet;
+        }
+
+        private void recreateNetButton_MouseEnter(object sender, EventArgs e)
+        {
+            infoStatusLabel.Text = "Заново пересоздаёт сеть с указанными параметрами";
+        }
+    }
+
+  }
---
+++ NeuralNetwork1/Neural.cs
@@ -0,0 +1,375 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace NeuralNetwork1
+{
+    /// <summary>
+    /// Класс для хранения образа – входной массив сигналов на сенсорах, выходные сигналы сети, и прочее
+    /// </summary>
+    public class Sample
+    {
+        /// <summary>
+        /// Входной вектор
+        /// </summary>
+        public double[] input = null;
+
+        /// <summary>
+        /// Выходной вектор, задаётся извне как результат распознавания
+        /// </summary>
+        public double[] output = null;
+
+        /// <summary>
+        /// Вектор ошибки, вычисляется по какой-нибудь хитрой формуле
+        /// </summary>
+        public double[] error = null;
+
+        /// <summary>
+        /// Действительный класс образа. Указывается учителем
+        /// </summary>
+        public FigureType actualClass;
+
+        /// <summary>
+        /// Распознанный класс - определяется после обработки
+        /// </summary>
+        public FigureType recognizedClass;
+
+        /// <summary>
+        /// Конструктор образа - на основе входных данных для сенсоров, при этом можно указать класс образа, или не указывать
+        /// </summary>
+        /// <param name="inputValues"></param>
+        /// <param name="sampleClass"></param>
+        public Sample(double[] inputValues, int classesCount, FigureType sampleClass = FigureType.Undef)
+        {
+            //  Клонируем массивчик
+            input = (double[])inputValues.Clone();
+            output = new double[classesCount];
+            if (sampleClass != FigureType.Undef) output[(int)sampleClass] = 1;
+
+            recognizedClass = FigureType.Undef;
+            actualClass = sampleClass;
+        }
+
+        /// <summary>
+        /// Обработка реакции сети на данный образ на основе вектора выходов сети
+        /// </summary>
+        public void processOutput()
+        {
+            if (error == null)
+                error = new double[output.Length];
+
+            //  Нам так-то выход не нужен, нужна ошибка и определённый класс
+            recognizedClass = 0;
+            for (int i = 0; i < output.Length; ++i)
+            {
+                error[i] = ((i == (int)actualClass ? 1 : 0) - output[i]);
+                if (output[i] > output[(int)recognizedClass]) recognizedClass = (FigureType)i;
+            }
+        }
+
+        /// <summary>
+        /// Вычисленная суммарная квадратичная ошибка сети. Предполагается, что целевые выходы - 1 для верного, и 0 для остальных
+        /// </summary>
+        /// <returns></returns>
+        public double EstimatedError()
+        {
+            double Result = 0;
+            for (int i = 0; i < output.Length; ++i)
+                Result += Math.Pow(error[i], 2);
+            return Result;
+        }
+
+        /// <summary>
+        /// Добавляет к аргументу ошибку, соответствующую данному образу (не квадратичную!!!)
+        /// </summary>
+        /// <param name="errorVector"></param>
+        /// <returns></returns>
+        public void updateErrorVector(double[] errorVector)
+        {
+            for (int i = 0; i < errorVector.Length; ++i)
+                errorVector[i] += error[i];
+        }
+
+        /// <summary>
+        /// Представление в виде строки
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            string result = "Sample decoding : " + actualClass.ToString() + "(" + ((int)actualClass).ToString() +
+                            "); " + Environment.NewLine + "Input : ";
+            for (int i = 0; i < input.Length; ++i) result += input[i].ToString() + "; ";
+            result += Environment.NewLine + "Output : ";
+            if (output == null) result += "null;";
+            else
+                for (int i = 0; i < output.Length; ++i)
+                    result += output[i].ToString() + "; ";
+            result += Environment.NewLine + "Error : ";
+
+            if (error == null) result += "null;";
+            else
+                for (int i = 0; i < error.Length; ++i)
+                    result += error[i].ToString() + "; ";
+            result += Environment.NewLine + "Recognized : " + recognizedClass.ToString() + "(" +
+                      ((int)recognizedClass).ToString() + "); " + Environment.NewLine;
+
+            return result;
+        }
+
+        /// <summary>
+        /// Правильно ли распознан образ
+        /// </summary>
+        /// <returns></returns>
+        public bool Correct() { return actualClass == recognizedClass; }
+    }
+
+    /// <summary>
+    /// Выборка образов. Могут быть как классифицированные (обучающая, тестовая выборки), так и не классифицированные (обработка)
+    /// </summary>
+    public class SamplesSet : IEnumerable
+    {
+        /// <summary>
+        /// Накопленные обучающие образы
+        /// </summary>
+        public List<Sample> samples = new List<Sample>();
+
+        /// <summary>
+        /// Добавление образа к коллекции
+        /// </summary>
+        /// <param name="image"></param>
+        public void AddSample(Sample image)
+        {
+            samples.Add(image);
+        }
+
+        public int Count { get { return samples.Count; } }
+
+        public IEnumerator GetEnumerator()
+        {
+            return samples.GetEnumerator();
+        }
+
+        /// <summary>
+        /// Реализация доступа по индексу
+        /// </summary>
+        /// <param name="i"></param>
+        /// <returns></returns>
+        public Sample this[int i]
+        {
+            get { return samples[i]; }
+            set { samples[i] = value; }
+        }
+
+        public double ErrorsCount()
+        {
+            double correct = 0;
+            double wrong = 0;
+            foreach (var sample in samples)
+                if (sample.Correct()) ++correct; else ++wrong;
+            return correct / (correct + wrong);
+        }
+
+        // Тут бы ещё сохранение в файл и чтение сделать, вообще классно было бы
+    }
+
+    public class NeuralNetwork : BaseNetwork
+    {
+        static Random rand = new Random();
+        private class NeuralNode
+        {
+            public double Value { get; set; }
+            public double Error { get; set; }
+            private List<NeuralLink> prevLayer;
+            private List<NeuralLink> nextLayer;
+
+            Func<double, double> activationFunction;
+
+            public NeuralNode(Func<double, double> activationFunction)
+            {
+                this.activationFunction = activationFunction;
+                prevLayer = new List<NeuralLink>();
+                nextLayer = new List<NeuralLink>();
+            }
+
+            public void LinkNextNode(NeuralNode next)
+            {
+                var link = new NeuralLink(this, next, 1.0);
+                this.nextLayer.Add(link);
+                next.prevLayer.Add(link);
+            }
+            public double Eval()
+            {
+                double sum = EvalInputSum();
+                Value = activationFunction.Invoke(sum);
+                return Value;
+            }
+
+            public void ReevalLinks(double learningRate)
+            {
+                Error = Error * Value * (1 - Value);
+                foreach (var link in prevLayer)
+                {
+                    link.prevNode.Error += Error * link.Weight;
+                    link.Weight += learningRate * Error * link.InputValue;
+                }
+            }
+
+            private double EvalInputSum() =>
+                prevLayer.Sum(link => link.OutputValue);
+        }
+
+        private class NeuralLink
+        {
+            public NeuralNode prevNode;
+            public NeuralNode nextNode;
+            public double Weight { get; set; }
+
+            public double InputValue
+            {
+                get { return prevNode.Value; }
+            }
+
+            public double OutputValue
+            {
+                get { return prevNode.Value * Weight; }
+            }
+
+            public NeuralLink(NeuralNode prevNode, NeuralNode nextNode, double weight = 1)
+            {                
+                this.prevNode = prevNode;
+                this.nextNode = nextNode;
+                this.Weight = rand.NextDouble() - 1.0;
+            }
+        }
+
+        private NeuralNode[][] nodeLayers;
+        private double learningRate;
+
+        public NeuralNetwork(int[] structure, double learningRate = 0.25)
+        {
+            this.learningRate = learningRate;
+            Init(structure);
+        }
+
+        public void Init(int[] structure)
+        {
+            Func<double, double> func = (double x) => {
+                return 1 / (1 + Math.Exp(-x));
+            };
+
+
+            //  Creating layer structure
+            nodeLayers = new NeuralNode[structure.Length][];
+            for (var i = 0; i < structure.Length; i++)
+            {
+                var layerCount = structure[i];
+                nodeLayers[i] = new NeuralNode[layerCount];
+                for (var j = 0; j < layerCount; j++)
+                    nodeLayers[i][j] = new NeuralNode(func);
+            }
+
+            // Linking nodes in adjacent layers
+            for (var i = 0; i < nodeLayers.Length - 1; i++)
+            {
+                var currentLayer = nodeLayers[i];
+                var nextLayer = nodeLayers[i + 1];
+
+                foreach (var node in currentLayer)
+                    foreach (var nextNode in nextLayer)
+                        node.LinkNextNode(nextNode);
+            }
+        }
+
+        public override void ReInit(int[] structure, double initialLearningRate = 0.25)
+        {
+            learningRate = initialLearningRate;
+            Init(structure);
+        }
+                
+        public override int Train(Sample sample, bool parallel = true)
+        {
+            var errorThreshold = 0.2;
+            for (int i = 0; i < 1000; i++)
+            {
+                Run(sample);
+                if (sample.Correct() && sample.EstimatedError() < errorThreshold)
+                    return i;
+
+                ReevalWeights(sample);
+            }
+            throw new Exception("Too hard to train");
+        }
+
+        public override double TrainOnDataSet(SamplesSet samplesSet, int epochs_count, double acceptable_erorr, bool parallel = true)
+        {
+            var guessLevel = 0.0;
+            while (epochs_count > 0)
+            {
+                foreach (var sample in samplesSet)
+                    if (Train((Sample)sample) == 0)
+                        guessLevel += 1;
+                guessLevel /= samplesSet.samples.Count;
+                if (guessLevel > acceptable_erorr) return guessLevel;
+                epochs_count--;
+            }
+            
+            return guessLevel;
+        }                        
+
+        public override FigureType Predict(Sample sample)
+        {
+            Run(sample);
+            return sample.recognizedClass;
+        }
+
+        public override double TestOnDataSet(SamplesSet testSet)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override double[] getOutput() =>        
+            nodeLayers.Last().Select(node => node.Value).ToArray();        
+
+        private void Run(Sample sample)
+        {
+            // Init first layer by sensors' values
+            for (var i = 0; i < sample.input.Length; i++)                
+                nodeLayers[0][i].Value = sample.input[i];
+
+            for (var i = 1; i < nodeLayers.Length; i++)
+                foreach (var node in nodeLayers[i])
+                    node.Eval();
+
+            // Copy output to sample
+            var lastLayer = nodeLayers.Last();
+            sample.output = new double[lastLayer.Length];
+            for (var i = 0; i < lastLayer.Length; i++)
+                sample.output[i] = lastLayer[i].Value;
+
+            sample.processOutput();
+        }
+
+        private void ReevalWeights(Sample sample)
+        {
+            //  Reset Errors
+            foreach (var layer in nodeLayers)
+                foreach (var node in layer)
+                    node.Error = 0.0;
+
+            //  Init errors on last layer
+            var lastLayer = nodeLayers.Last();
+            for (int i = 0; i < lastLayer.Length; i++)
+                lastLayer[i].Error = sample.error[i];
+
+            foreach (var layer in nodeLayers.Reverse())
+                foreach (var node in layer)
+                    node.ReevalLinks(learningRate);            
+        }
+    }    
+}
